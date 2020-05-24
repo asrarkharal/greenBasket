f67582205f4c46fc67f08806a165b60d53a24dcf
Asrar <threeman@yahoo.com>
2020-05-24 13:59:38 +0200

    small changes in admin index

--- Project Ecommerce/public/admin/index.php
+++ Project Ecommerce/public/admin/index.php
@@ -8,11 +8,23 @@
 <body>
     <h1>Admin page</h1>
     This page should be active if Admin login success
+
+    <h3>Products</h3>
     <ul>
         <li><a href="">Edit</a></li>
         <li><a href="">Delete</a></li>
         <li><a href="">Create</a></li>
     </ul>
+
+
+    <h3>User</h3>
+    <ul>
+        <li><a href="">Edit</a></li>
+        <li><a href="">Delete</a></li>
+        <li><a href="">Create</a></li>
+    </ul>
+
+
 </body>
 
 </html>
