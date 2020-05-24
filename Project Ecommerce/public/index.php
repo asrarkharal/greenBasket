78d497754eae1b6e456acb25af3a93256951c4b9
Asrar <threeman@yahoo.com>
2020-05-24 13:38:15 +0200

    project template updated

--- Project Ecommerce/public/index.php
+++ Project Ecommerce/public/index.php
@@ -10,6 +10,22 @@ include('../src/config.php');
 </head>
 
 <body>
+    <header>
+        <nav>
+            <a href="">User Login</a> <span> | </span> <a href="">Register</a>
+
+        </nav>
+
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="user/index.php">User Index</a></li>
+                <li><a href="admin/index.php">admin Index</a></li>
+
+            </ul>
+
+        </nav>
+    </header>
     <h1>Shop page</h1>
 
     <?php
--- Project Ecommerce/public/user/index.php
+++ Project Ecommerce/public/user/index.php
@@ -2,17 +2,19 @@
 <html>
 
 <head>
-    <title>Admin</title>
+    <title>User</title>
 </head>
 
 <body>
-    <h1>Admin page</h1>
-    This page should be active if Admin login success
+    <h1>User page</h1>
+    This page should be active if user login success
+
     <ul>
         <li><a href="">Edit</a></li>
         <li><a href="">Delete</a></li>
         <li><a href="">Create</a></li>
     </ul>
+
 </body>
 
 </html>
