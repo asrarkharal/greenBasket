b9910a7b0185aef06a3e8f1a2442c0ce0a88b776
Asrar <threeman@yahoo.com>
2020-06-12 15:03:02 +0200

    improving product dashbord

--- Project Ecommerce/public/admin/edit_product_page.php
+++ Project Ecommerce/public/admin/edit_product_page.php
@@ -42,49 +42,63 @@ if (isset($_POST['updateProductBtn'])) {
 }
 ?>
 
-<div class="container">
-    <div class="contact-form spad">
-        <div class="col-lg-9">
-            <div class="contact__form__title">
-                <h2>Edit Product Page</h2>
+<div class="main">
+    <section class="hero">
+        <div class="container">
+            <div class="content">
+                <div class="contact-form spad">
+                    <div class="col-lg-9">
+                        <div class="contact__form__title">
+                            <h2>Edit Product Page</h2>
+                        </div>
+                    </div>
+
+
+                    <h5><?= $errorUl ?></h5>
+
+                    <div class="col-sm-12 col-md-12 col-lg-9">
+                        <form method="POST" action="#" id="edit-form-inputs">
+                            <fieldset>
+                                <legend>Update Product</legend>
+
+                                <p>
+                                    <label for="input1">Title:</label> <br>
+                                    <input type="text" class="text" name="productTitle" value="<?= $title ?>">
+                                </p>
+                                <p>
+                                    <label for="input1">Description:</label> <br>
+                                    <input type="text" class="text" name="productDescription"
+                                        value="<?= $description ?>">
+                                </p>
+                                <p>
+                                    <label for="input1">Price:</label> <br>
+                                    <input type="text" class="text" name="productPrice" value="<?= $price ?>">
+                                </p>
+                                <p>
+                                    <label for="input1">Img Url:</label> <br>
+                                    <input type="text" class="text" name="imgUrl" value="<?= $imgurl ?>">
+                                    <input type="hidden" class="text" name="productId" value="<?= $id ?>">
+                                </p>
+                                <p>
+                                    <input type="submit" name="updateProductBtn" value="Update Product"
+                                        class="btn btn-success">
+                                </p>
+                            </fieldset>
+                        </form>
+                        <a href="index.php">Back To Product Edit Page</a>
+                    </div>
+
+                </div>
             </div>
+
+            <section id="myfooter">
+                <!-- Footer -->
+                <?php include('layout/footer.php'); ?>
+            </section>
+
         </div>
-
-
-        <h5><?= $errorUl ?></h5>
-
-        <div class="col-sm-12 col-md-12 col-lg-9">
-            <form method="POST" action="#">
-                <fieldset>
-                    <legend>Update Product</legend>
-
-                    <p>
-                        <label for="input1">Title:</label> <br>
-                        <input type="text" class="text" name="productTitle" value="<?= $title ?>">
-
-                        <label for="input1">Description:</label> <br>
-                        <input type="text" class="text" name="productDescription" value="<?= $description ?>">
-
-                        <label for="input1">Price:</label> <br>
-                        <input type="text" class="text" name="productPrice" value="<?= $price ?>">
-
-                        <label for="input1">Img Url:</label> <br>
-                        <input type="text" class="text" name="imgUrl" value="<?= $imgurl ?>">
-                        <input type="hidden" class="text" name="productId" value="<?= $id ?>">
-                    </p>
-                    <p>
-                        <input type="submit" name="updateProductBtn" value="Update Product">
-                    </p>
-                </fieldset>
-            </form>
-        </div>
-
-    </div>
+    </section>
 </div>
 
 
 <a href="index.php">Go back</a>
-
-<?php
-include('layout/footer.php')
-?>
